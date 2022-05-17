fe7ce94b5a11f3a7e8d3155286125661dc68801f
Hellen Torres <104654027+Hellentorresr@users.noreply.github.com>
2022-05-17 15:58:40 -0600

    Delete TestOne.java

--- TestOne.java
+++
@@ -1,8 +0,0 @@
-public class TestOne {
-    public static void main(String[] args) {
-        System.out.println("Hi there");
-        System.out.println("ji");//k;l
-
-        System.out.println("test");
-    }
-}
