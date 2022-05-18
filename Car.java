0a3c92a44fcdb6aba4f07c2fa1206b10dc73ef84
HellenTorres <104654027+Hellentorresr@users.noreply.github.com>
2022-05-18 15:12:54 -0600

    nuevos cambios

--- Car.java
+++ Car.java
@@ -1,20 +1,99 @@
 
 public class Car{
-    int rueda;
-    int largo;
-    int ancho;
-    int motor;
-    int peso;
+   private int rueda;
+    private int largo;
+   private int ancho;
+    private int motor;
+    private int peso_plataforma;
+   private String color;
+   private int peso_total;
+   private boolean asientos_cuero, climatizador;
 
-    //nombre
 
-    public Car(){
+   //Constructor
+   public Car(){
         rueda = 4;
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        peso_plataforma = 500;
     }
 
+    //Getter method//here quiero que regrese un entero pero con una frase
+    //para esto se usa tipo de dato String
+    public String dime_Datos_Generales(){ //GETTER
+        return "\nLa plataforma del vehiculo tiene: "+rueda +" ruedas" + ". Mide "+ largo/1000 + " Metros con un ancho de " +
+                + ancho  + " Centimetres y un peso de Plataforma de: "+ peso_plataforma +
+                " Kilos.";
+    }
+
+    //Setter
+    public void estable_color(String color_carro){
+       color = color_carro;
+    }
+
+    //getter para saber el color
+    public String dimeColor(){
+        return "El color del coche es: "+color;
+    }
+
+    //method getter and setter asientos de cuero
+    public void configura_asientos(String pAsientos_Cuero){ //setter
+       if(pAsientos_Cuero == "si"){
+           asientos_cuero = true;
+       }else {
+           asientos_cuero = false;
+       }
+    }
+
+    public String dime_asientos(){ //GETTER
+       if (asientos_cuero == true){
+           return "El coche tiene asientos de cuero";
+       }else {
+           return "El coche no tiene asientos de cuero";
+       }
+    }
+
+    public void config_climatizador(String pClimatizador){ //setter
+        if(pClimatizador == "si"){
+            climatizador = true;
+        }else {
+            climatizador = false;
+        }
+    }
+
+    public String dime_climatizador(){ // getter
+       if (climatizador == true){
+           return "El coche incorpora climatizador";
+       }else {
+           return "El coche no incorpora climatizador";
+       }
+    }
+
+    public String dime_peso_coche(){ //setter and getter no a good practice
+      int pesoCarrocería =500;
+      peso_total = peso_plataforma+pesoCarrocería;
+
+      if (asientos_cuero == true){
+          peso_total+= 50;
+      }
+      if(climatizador == true){
+          peso_total+= 20;
+      }
+      return "El peso del coche es: "+peso_total;
+    }
+
+    public int precio_coche(){ //getter
+       int precioFinal= 10000;
+
+       if (asientos_cuero == true){
+           precioFinal+= 2000;
+       }
+
+       if (climatizador == true){
+           precioFinal += 1500;
+       }
+       return precioFinal;
+    }
 }
 
--- Uso_Coche.java
+++ Uso_Coche.java
@@ -4,6 +4,20 @@ public class Uso_Coche {
         //nuevo objeto
         Car car1 = new Car();
         //modificar o acceder la propiedad
-        System.out.println("Este carro tiene: "+car1.rueda+ " Ruedas");
+        //System.out.println("Este carro tiene: "+car1.rueda+ " Ruedas");//con el modifier prívate no se puede hacer esto
+
+        System.out.println(car1.dime_Datos_Generales());
+        car1.estable_color("Negro");
+        System.out.println(car1.dimeColor());
+
+        car1.configura_asientos("no");
+        System.out.println(car1.dime_asientos());
+
+        car1.config_climatizador("si");
+        System.out.println(car1.dime_climatizador());
+
+        System.out.println(car1.dime_peso_coche());
+
+        System.out.println("El final de coche es: "+ car1.precio_coche());
     }
 }
