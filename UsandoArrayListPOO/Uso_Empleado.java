3754b81a7c4cf77b817a02b8ce4266db2ad28d79
HellenTorres <104654027+Hellentorresr@users.noreply.github.com>
2022-06-02 17:35:37 -0600

    nuevos cambios

--- UsandoArrayListPOO/Uso_Empleado.java
+++ UsandoArrayListPOO/Uso_Empleado.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
 
 public class Uso_Empleado {
     public static void main(String[] args) {
-        ArrayList<Empleado> listaEmpleados = new ArrayList <Empleado>();
+        ArrayList<Empleado> listaEmpleados = new ArrayList <>();
 
         // Adding elements to the arrayList
         listaEmpleados.add(new Empleado("Laura", 25, 800));
         listaEmpleados.add(new Empleado("Maria", 20, 200));
-        listaEmpleados.add(new Empleado("Meghan", 7, 800));
+        listaEmpleados.add(new Empleado("Meghan",7, 800));
 
         //Empleado uno = new Empleado("Juana", 15, 18);
         System.out.println(listaEmpleados.size());
