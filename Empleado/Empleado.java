8636111e9a1c7586be5e323a1b855ff747f0195b
Hellentorresr <htorresr1@ucenfotec.ac.cr>
2022-06-07 10:19:19 -0600

    Actualizando

--- Empleado/Empleado.java
+++ Empleado/Empleado.java
@@ -4,45 +4,45 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Empleado {
- // variable de clases atributos
+    // variable de clases atributos
     private final String nom;
-    private  double sueldo;
     private final Date altaContrato; // tipo de dato Date tipo objeto
+    private double sueldo;
 
 
     //Constructor
-    public Empleado(String nom, double sueldo, int year, int mes, int day){ //con los tres últimos calcular el dia de alta
+    public Empleado(String nom, double sueldo, int year, int mes, int day) { //con los tres últimos calcular el dia de alta
         this.nom = nom;
         this.sueldo = sueldo;
-        GregorianCalendar calendar = new GregorianCalendar(year, mes-1, day);
+        GregorianCalendar calendar = new GregorianCalendar(year, mes - 1, day);
         //para que el objeto me devuelva la fecha usamos el method
         altaContrato = calendar.getTime();
     }
 
-    public String getName(){ //getter
+    public String getName() { //getter
         return nom;
     }
 
-    public double getSalary(){ //getter
+    public double getSalary() { //getter
         return sueldo;
     }
 
-    public Date getFecha_Contrato(){ //getter
+    public Date getFecha_Contrato() { //getter
         return altaContrato;
     }
 
     // Metodo que suba el sueldo
-    public void subeSueldo(double porcentaje){ //setter
-        double aumento = (sueldo * porcentaje) /100; //calculamos el porcentaje
-        sueldo+=aumento; //El sueldo más el porcentaje de aumento
+    public void subeSueldo(double porcentaje) { //setter
+        double aumento = (sueldo * porcentaje) / 100; //calculamos el porcentaje
+        sueldo += aumento; //El sueldo más el porcentaje de aumento
     }
 
     @Override
     public String toString() {
         return "Empleado{" +
-                "Nombre= " + nom + '\'' +
-                ", Sueldo= " + sueldo +
-                ", Fecha de alta= " + altaContrato +
+                "Nombre = " + nom + '\'' +
+                ", Sueldo = " + sueldo +
+                ", Fecha de alta = " + altaContrato +
                 '}';
     }
 }
--- Empleado/Uso_Empleado.java
+++ Empleado/Uso_Empleado.java
@@ -1,39 +1,32 @@
 package Empleado;
-
-
 public class Uso_Empleado {
     public static void main(String[] args) {
-        //Object 1
-       /* Empleado primerEmpleado = new Empleado("Hellen",85000,2020,1,10);
-        primerEmpleado.subeSueldo(5);
-        System.out.println(primerEmpleado);
-
-        //Object 2
-        Empleado segundoEmpleado = new Empleado("Meghan",10000,2015,6,1);
-        segundoEmpleado.subeSueldo(10);
-
-        //Object 2
-        Empleado tercerEmpleado = new Empleado("Miguel",800,2020,12,15);
-        segundoEmpleado.subeSueldo(20);*/
-
-
-        // Faster way to create objects
         // array
         Empleado[] misEmpleados = new Empleado[6]; //array de tres
 
-        //Filling up the array
-        misEmpleados[0] = new Empleado("Meghan",10000,2015,6,1);
-        misEmpleados[1] = new Empleado("Miguel",800,2020,12,15);
-        misEmpleados[2] = new Empleado("Hellen",85000,2020,1,10);
-        misEmpleados[3] = new Empleado( "chepe", 55000, 2022, 2, 12);
-        misEmpleados[4] = new Empleado( "chepe", 55000, 2022, 2, 12);
-        misEmpleados[5] = new Empleado( "chepe", 55000, 2022, 2, 12);
-        for (Empleado i: misEmpleados) {
+        /*
+        Filling up the array de misEmpleados
+         */
+        misEmpleados[0] = new Empleado("Meghan", 10000, 2015, 6, 1);
+        misEmpleados[1] = new Empleado("Miguel", 800, 2020, 12, 15);
+        misEmpleados[2] = new Empleado("Hellen", 85000, 2020, 1, 10);
+        misEmpleados[3] = new Empleado("chepe", 55000, 2022, 2, 12);
+        misEmpleados[4] = new Empleado("chepe", 55000, 2022, 2, 12);
+        misEmpleados[5] = new Empleado("chepe", 55000, 2022, 2, 12);
+
+        /*
+         *  usamos esta estructura de control forEach para recorrer
+         *  El arreglo mis empleados
+         */
+        for (Empleado i : misEmpleados) {
             //usando un metodo de la clase Empleado
             i.subeSueldo(5);//sube sueldo a los tres
         }
 
-        for (Empleado i:misEmpleados){
+        /*
+        Imprimiendo el array de objetos misEmpleados
+         */
+        for (Empleado i : misEmpleados) {
             System.out.println("---------------");
             System.out.println(i);
             System.out.println("---------------");
